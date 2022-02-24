5c28b1b8796a3b1954fdc3b61dea1efaae458cb1
Javier Pérez López <j.perezlopez@student.tudelft.nl>
2022-02-24 20:15:34 +0000

    Add API endpoints for scores

--- server/src/main/java/server/api/ScoreController.java
+++ server/src/main/java/server/api/ScoreController.java
@@ -17,16 +17,10 @@ package server.api;
 
 import java.util.*;
 
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import commons.Quote;
-import server.database.QuoteRepository;
 
 @RestController
 @RequestMapping("/api/scores")
