8f3b02585151e2d3c7e9af5fa3c3ebbe8b810c95
Piotr Politowicz <P.A.Politowicz@student.tudelft.nl>
2022-03-03 18:18:03 +0100

    Fix the CheckStyle issue Fixed a star import.

--- commons/src/main/java/commons/Question.java
+++ commons/src/main/java/commons/Question.java
@@ -1,6 +1,10 @@
 package commons;
 
-import javax.persistence.*;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.SequenceGenerator;
 import java.util.Objects;
 
 @Entity
