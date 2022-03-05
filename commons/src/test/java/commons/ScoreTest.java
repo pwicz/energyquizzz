5a33967df7f99e750f4d1816ced4404f07b29e56
Piotr Politowicz <P.A.Politowicz@student.tudelft.nl>
2022-03-05 11:16:11 +0100

    Add proper tests for Score entity

---
+++ commons/src/test/java/commons/ScoreTest.java
@@ -0,0 +1,43 @@
+package commons;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class ScoreTest {
+    private static final String PLAYERNAME = "Peter";
+    private static final int PLAYERSCORE = 9999;
+
+    @Test
+    public void testConstructor(){
+        Score s = new Score(PLAYERNAME, PLAYERSCORE);
+        assertEquals(PLAYERNAME, s.playerName);
+        assertEquals(PLAYERSCORE, s.playerScore);
+    }
+
+    @Test
+    public void testEqualHashCode(){
+        Score s1 = new Score(PLAYERNAME, PLAYERSCORE);
+        Score s2 = new Score(PLAYERNAME, PLAYERSCORE);
+
+        assertEquals(s1.hashCode(), s2.hashCode());
+    }
+
+    @Test
+    public void testNotEqualHashCode(){
+        Score s1 = new Score(PLAYERNAME + "a", PLAYERSCORE + 12);
+        Score s2 = new Score(PLAYERNAME, PLAYERSCORE);
+
+        assertNotEquals(s1.hashCode(), s2.hashCode());
+    }
+
+    @Test
+    public void testToString(){
+        Score s = new Score(PLAYERNAME, PLAYERSCORE);
+        String scoreString = s.toString();
+
+        assertTrue(scoreString.contains(Score.class.getSimpleName()));
+        assertTrue(scoreString.contains(PLAYERNAME));
+        assertTrue(scoreString.contains(Integer.toString(PLAYERSCORE)));
+    }
+}
