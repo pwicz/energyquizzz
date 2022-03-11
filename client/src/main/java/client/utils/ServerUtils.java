db78ad57b3c3135d51f2015854514a7ea687bce1
Piotr Politowicz <P.A.Politowicz@student.tudelft.nl>
2022-03-11 11:39:07 +0100

    Add proper ServerUtils methods Added methods to use WebSockets from the client-side.

--- client/src/main/java/client/utils/ServerUtils.java
+++ client/src/main/java/client/utils/ServerUtils.java
@@ -20,8 +20,11 @@ import static jakarta.ws.rs.core.MediaType.APPLICATION_JSON;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.lang.reflect.Type;
 import java.net.URL;
 import java.util.List;
+import java.util.concurrent.ExecutionException;
+import java.util.function.Consumer;
 
 import org.glassfish.jersey.client.ClientConfig;
 
@@ -29,6 +32,13 @@ import commons.Quote;
 import jakarta.ws.rs.client.ClientBuilder;
 import jakarta.ws.rs.client.Entity;
 import jakarta.ws.rs.core.GenericType;
+import org.springframework.messaging.converter.MappingJackson2MessageConverter;
+import org.springframework.messaging.simp.stomp.StompFrameHandler;
+import org.springframework.messaging.simp.stomp.StompHeaders;
+import org.springframework.messaging.simp.stomp.StompSession;
+import org.springframework.messaging.simp.stomp.StompSessionHandlerAdapter;
+import org.springframework.web.socket.client.standard.StandardWebSocketClient;
+import org.springframework.web.socket.messaging.WebSocketStompClient;
 
 public class ServerUtils {
 
@@ -59,4 +69,44 @@ public class ServerUtils {
                 .accept(APPLICATION_JSON) //
                 .post(Entity.entity(quote, APPLICATION_JSON), Quote.class);
     }
+
+    private StompSession session = connect("ws://localhost:8080/websocket");
+
+    private StompSession connect(String url){
+        var client = new StandardWebSocketClient();
+        var stomp = new WebSocketStompClient(client);
+        stomp.setMessageConverter(new MappingJackson2MessageConverter());
+
+        try{
+            return stomp.connect(url, new StompSessionHandlerAdapter() {}).get();
+        }
+        catch(ExecutionException e){
+            throw new RuntimeException(e);
+        }
+        catch(InterruptedException e){
+            Thread.currentThread().interrupt();
+        }
+
+        throw new IllegalStateException();
+    }
+
+    public <T> void registerForMessage(String dest, Class<T> type, Consumer<T> consumer){
+        // set up the connection
+        session.subscribe(dest, new StompFrameHandler() {
+
+            @Override
+            public Type getPayloadType(StompHeaders headers) {
+                return type; // we expect to get something of type 'type'
+            }
+
+            @Override
+            public void handleFrame(StompHeaders headers, Object payload) {
+                // we know that 'payload' will be of type T, because Jackson Converter
+                // takes care of that
+                consumer.accept((T) payload);
+            }
+        });
+    }
+
+    public void send(String dest, Object o){ session.send(dest, o); }
 }
