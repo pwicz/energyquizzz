ec337006aa6bc5cae47ff19496803d00e9b46528
Justin Wisker <J.P.T.Wisker@student.tudelft.nl>
2022-03-13 15:38:53 +0100

    Add functionallity to the singleplayer leaderboard

--- client/src/main/java/client/scenes/MainCtrl.java
+++ client/src/main/java/client/scenes/MainCtrl.java
@@ -36,9 +36,12 @@ public class MainCtrl {
     private Scene question;
     private  MultiplayerScreenCtrl multiplayerScreenCtrl;
 
+    private Scene singleLeaderboard;
+    private SingleplayerLeaderboardCtrl singleplayerLeaderboardCtrl;
+
     public void initialize(Stage primaryStage, Pair<QuoteOverviewCtrl, Parent> overview,
                            Pair<AddQuoteCtrl, Parent> add, Pair<SplashScreenCtrl, Parent> splash,
-                           Pair<MultiplayerScreenCtrl, Parent> question) {
+                           Pair<MultiplayerScreenCtrl, Parent> question, Pair<SingleplayerLeaderboardCtrl, Parent> singleLeaderboard) {
         this.primaryStage = primaryStage;
         this.overviewCtrl = overview.getKey();
         this.overview = new Scene(overview.getValue());
@@ -51,6 +54,9 @@ public class MainCtrl {
 
         this.question = new Scene(question.getValue());
         this.multiplayerScreenCtrl = question.getKey();
+
+        this.singleLeaderboard = new Scene(singleLeaderboard.getValue());
+        this.singleplayerLeaderboardCtrl = singleLeaderboard.getKey();
 
         showOverview();
         primaryStage.show();
@@ -77,6 +83,12 @@ public class MainCtrl {
     public void showMultiplayerScreen() {
         primaryStage.setTitle("Multiplayer");
         primaryStage.setScene(question);
+        add.setOnKeyPressed(e -> addCtrl.keyPressed(e));
+    }
+
+    public void showSingleLeaderboardScreen() {
+        primaryStage.setTitle("SingleplayerLeaderboard");
+        primaryStage.setScene(singleLeaderboard);
         add.setOnKeyPressed(e -> addCtrl.keyPressed(e));
     }
 
--- client/src/main/java/client/scenes/SingleplayerLeaderboardCtrl.java
+++ client/src/main/java/client/scenes/SingleplayerLeaderboardCtrl.java
@@ -1,4 +1,47 @@
 package client.scenes;
 
+import client.utils.ServerUtils;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ListView;
+import javafx.scene.control.TextField;
+
+import javax.inject.Inject;
+
 public class SingleplayerLeaderboardCtrl {
+
+    private final ServerUtils server;
+    private final MainCtrl mainCtrl;
+
+    @FXML
+    Button start;
+
+    @FXML
+    Button leave;
+
+    @FXML
+    TextField nameField;
+
+    @FXML
+    ListView leaderboard;
+
+
+
+    @Inject
+    public SingleplayerLeaderboardCtrl(ServerUtils server, MainCtrl mainCtrl) {
+        this.mainCtrl = mainCtrl;
+        this.server = server;
+    }
+
+    public void start() { //start a singleplayer game
+    }
+
+    public void insertName() {
+        String name =nameField.getText();
+        System.out.println(name); // You need to press enter when inserting you name.
+    }
+
+    public void leave(){
+        mainCtrl.showSplash();
+    }
 }
