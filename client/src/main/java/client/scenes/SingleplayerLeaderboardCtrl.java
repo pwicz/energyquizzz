b6142c27bb531ec9dd2b527dfaecdc0199e7d156
Justin Wisker <J.P.T.Wisker@student.tudelft.nl>
2022-03-17 17:22:43 +0100

    Make the leaderboard get the scores from the server

--- client/src/main/java/client/scenes/SingleplayerLeaderboardCtrl.java
+++ client/src/main/java/client/scenes/SingleplayerLeaderboardCtrl.java
@@ -5,8 +5,17 @@ import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.ListView;
 import javafx.scene.control.TextField;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.client.RestTemplate;
 
 import javax.inject.Inject;
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Stream;
 
 public class SingleplayerLeaderboardCtrl {
 
@@ -48,8 +57,16 @@ public class SingleplayerLeaderboardCtrl {
         System.out.println(name); // You need to press enter when inserting you name.
     }
 
-    public void insertLeaderboard() { //needs to change to import the database leaderboard
-        leaderboard.getItems().addAll("Justin", "Piotr", "Mike", "Ioana", "Alex");
+    public void insertLeaderboard() throws MalformedURLException { //needs to change to import the database leaderboard
+        RestTemplate restTemplate = new RestTemplate();
+        ResponseEntity<int[]> response = restTemplate.getForEntity("http://localhost:8080/api/scores/get5TopScores",
+                int[].class);
+        int[] scores = response.getBody();
+        ArrayList<String> topScores = new ArrayList<>();
+        for(int i = 0; i<scores.length;i++) {
+            topScores.add(String.valueOf(scores[i]));
+        }
+        leaderboard.getItems().addAll(topScores);
     }
 
     public void leave(){
