22b0fb50450fca9b211db6ba28b3c0dba3797651
Piotr Politowicz <P.A.Politowicz@student.tudelft.nl>
2022-03-17 14:59:27 +0100

    Add getters and setters and String id

--- commons/src/main/java/commons/Game.java
+++ commons/src/main/java/commons/Game.java
@@ -5,10 +5,30 @@ import java.util.List;
 public class Game {
 
     private List<Player> players;
-    private long ID;
+    private String ID;
 
-    public Game(List<Player> players, long ID) {
+    public Game(List<Player> players, String ID) {
         this.players = players;
+        this.ID = ID;
+    }
+
+    public void addPlayer(Player p){
+        players.add(p);
+    }
+
+    public List<Player> getPlayers() {
+        return players;
+    }
+
+    public void setPlayers(List<Player> players) {
+        this.players = players;
+    }
+
+    public String getID() {
+        return ID;
+    }
+
+    public void setID(String ID) {
         this.ID = ID;
     }
 }
