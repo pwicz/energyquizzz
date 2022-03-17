fb0136d02fa6e41668a3bf896ec5d70ec02782d2
Piotr Politowicz <P.A.Politowicz@student.tudelft.nl>
2022-03-17 21:56:35 +0100

    Add Javadocs

--- client/src/main/java/client/scenes/SingleplayerLeaderboardCtrl.java
+++ client/src/main/java/client/scenes/SingleplayerLeaderboardCtrl.java
@@ -9,38 +9,61 @@ import javafx.scene.control.TextField;
 
 import javax.inject.Inject;
 
+/**
+ * The type Singleplayer leaderboard ctrl.
+ */
 public class SingleplayerLeaderboardCtrl {
 
     private final ServerUtils server;
     private final MainCtrl mainCtrl;
 
+    /**
+     * The Start.
+     */
     @FXML
     Button start;
 
+    /**
+     * The Leave.
+     */
     @FXML
     Button leave;
 
+    /**
+     * The Name field.
+     */
     @FXML
     TextField nameField;
 
+    /**
+     * The Leaderboard.
+     */
     @FXML
     ListView<String> leaderboard;
 
-
-
+    /**
+     * Instantiates a new Singleplayer leaderboard ctrl.
+     *
+     * @param server   the server
+     * @param mainCtrl the main ctrl
+     */
     @Inject
     public SingleplayerLeaderboardCtrl(ServerUtils server, MainCtrl mainCtrl) {
         this.mainCtrl = mainCtrl;
         this.server = server;
     }
 
+    /**
+     * Will load the leaderboard.
+     */
     public void initialize() {
         start.fire(); //this loads the leaderboard, it is currently bound to the start button
     }
 
-
-    public void start() { //start a singleplayer game
-        System.out.println("test");
+    /**
+     * Start a singleplayer game.
+     */
+    public void start() {
         String playerName = nameField.getText();
         if(playerName == null || playerName.isEmpty()){
             return;
@@ -50,15 +73,24 @@ public class SingleplayerLeaderboardCtrl {
         server.send("/app/general", msg);
     }
 
+    /**
+     * Insert name.
+     */
     public void insertName() {
-        String name =nameField.getText();
+        String name = nameField.getText();
         System.out.println(name); // You need to press enter when inserting you name.
     }
 
+    /**
+     * Insert leaderboard.
+     */
     public void insertLeaderboard() { //needs to change to import the database leaderboard
         leaderboard.getItems().addAll("Justin", "Piotr", "Mike", "Ioana", "Alex");
     }
 
+    /**
+     * Leave.
+     */
     public void leave(){
         mainCtrl.showSplash();
     }
