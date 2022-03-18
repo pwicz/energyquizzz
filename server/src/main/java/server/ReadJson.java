93dbe529776070f879da031e4247e8b5e886c82e
Alexandra <a.velicu@student.tudelft.nl>
2022-03-18 17:15:24 +0200

    read JSON file --> activity list

---
+++ server/src/main/java/server/ReadJson.java
@@ -0,0 +1,29 @@
+package server;
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import commons.Activity;
+
+public class ReadJson {
+
+    private List<Activity> activities= new ArrayList<>();
+    public void readFile() throws InterruptedException{
+
+        ObjectMapper mapper = new ObjectMapper();
+        mapper.enable(SerializationFeature.INDENT_OUTPUT);
+        try {
+            activities = mapper.readValue(new File("package.json"), new TypeReference<List<Activity>>(){});
+        }catch(IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+
+
+}
