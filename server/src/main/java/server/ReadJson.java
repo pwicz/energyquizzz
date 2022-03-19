821604ff51adc71b4040b599ced441cf8ff83daa
Piotr Politowicz <P.A.Politowicz@student.tudelft.nl>
2022-03-19 11:42:21 +0100

    Fix unused import

--- server/src/main/java/server/ReadJson.java
+++ server/src/main/java/server/ReadJson.java
@@ -4,7 +4,6 @@ import java.io.IOException;
 import java.net.URISyntaxException;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Objects;
 
 import com.fasterxml.jackson.core.Version;
 import com.fasterxml.jackson.core.type.TypeReference;
