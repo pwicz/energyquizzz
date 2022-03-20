6ccd3c023fd9ed0429dc0e9dc9a009f6231efcc1
Piotr Politowicz <P.A.Politowicz@student.tudelft.nl>
2022-03-20 11:46:25 +0100

    Add correctAnswerID and getters/setters

--- commons/src/main/java/commons/Game.java
+++ commons/src/main/java/commons/Game.java
@@ -1,12 +1,14 @@
 package commons;
 
 import java.util.List;
+import java.util.Objects;
 
 public class Game {
 
     private List<Player> players;
     private String ID;
     private boolean isMultiplayer;
+    private Long correctAnswerID;
 
     public Game(List<Player> players, String ID) {
         this.players = players;
@@ -39,5 +41,21 @@ public class Game {
 
     public void setMultiplayer(boolean multiplayer) {
         isMultiplayer = multiplayer;
+    }
+
+    public Player getPlayerWithID(String playerID){
+        for(var p : players){
+            if(Objects.equals(p.getID(), playerID)) return p;
+        }
+
+        return null;
+    }
+
+    public Long getCorrectAnswerID() {
+        return correctAnswerID;
+    }
+
+    public void setCorrectAnswerID(Long correctAnswerID) {
+        this.correctAnswerID = correctAnswerID;
     }
 }
