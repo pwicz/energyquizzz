8ee991a97b54b6dd1d2ffc193881842e36b4a199
Javier Pérez López <j.perezlopez@student.tudelft.nl>
2022-03-23 18:07:50 +0100

    Add tests for UtilController
    -Add javadoc for both files

--- server/src/main/java/server/api/UtilController.java
+++ server/src/main/java/server/api/UtilController.java
@@ -10,6 +10,10 @@ import java.util.UUID;
 @RequestMapping("/api/util")
 public class UtilController {
 
+    /**
+     * Creates a unique ID for a player
+     * @return the newly created player ID
+     */
     @GetMapping("/getPlayerID")
     public String getPlayerID() {
         return UUID.randomUUID().toString();
