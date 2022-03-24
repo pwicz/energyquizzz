7c1242ad0b329b3ed2366bca1b336e56447efaef
Javier Pérez López <j.perezlopez@student.tudelft.nl>
2022-03-24 01:13:08 +0100

    Fix checkstyle issue

--- server/src/test/java/server/api/ActivityControllerTest.java
+++ server/src/test/java/server/api/ActivityControllerTest.java
@@ -8,7 +8,11 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class ActivityControllerTest {
 
