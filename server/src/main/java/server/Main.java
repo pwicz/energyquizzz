b6182b080dfaaa4987219ce3067934a54b5a5a68
Javier Pérez López <j.perezlopez@student.tudelft.nl>
2022-04-03 11:44:29 +0200

    Refactor if-else

--- server/src/main/java/server/Main.java
+++ server/src/main/java/server/Main.java
@@ -27,18 +27,15 @@ public class Main {
 
     public static void main(String[] args) throws InterruptedException {
         var context = SpringApplication.run(Main.class, args);
-        readJson = context.getBean(ReadJson.class);
+
 
         if(args.length > 0 && args[0].equals("load")){
-            System.out.println("load selected");
+            System.out.println("Load activities");
+            readJson = context.getBean(ReadJson.class);
+            readJson.readFile();
+            readJson.saveAllToDB();
         }else{
-            System.out.println("Not load");
+            System.out.println("Don't load activities");
         }
-
-        // Right now we add activities everytime we start the server - this must be changed
-        // later, as the example backlog says so. We will allow players to reload the activities
-        // from an admin panel
-        readJson.readFile();
-        readJson.saveAllToDB();
     }
 }
